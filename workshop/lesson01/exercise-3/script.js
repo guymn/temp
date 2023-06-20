f818d47f9c51debb3c4a5a2e4266e70cc7d8257b
guymn <gjp2267@gmail.com>
2023-06-20 20:52:26 +0700

    Lesson1 ex.3

--- workshop/lesson01/exercise-3/script.js
+++ workshop/lesson01/exercise-3/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const massMark = 1.88;
+const heightMark = 95;
+
+const massJohn = 1.76;
+const heightJohn = 85;
+
+const BMI_Mark = massMark / heightMark
+const BMI_John = massJohn / heightJohn
+
+const markHigherBMI = BMI_Mark > BMI_John
+
+console.log(markHigherBMI)
