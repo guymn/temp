fe7a4e48e5214fd48297f68cdd1d55a899fb3b01
guymn <gjp2267@gmail.com>
2023-06-20 20:41:14 +0700

    Lesson1 ex.1-2

--- workshop/lesson01/exercise-1/script.js
+++ workshop/lesson01/exercise-1/script.js
@@ -1,2 +1,37 @@
 'use strict';
 
+let nameShop = 'CAMT Stationery Shop'
+let date = '10-01-2023'
+let order = 'Order No.#1011'
+let nameUser = 'Mr.John Doe'
+let barrier = '--------------------------------------'
+let headTable = 'No  Description   QTY   Price   Total'
+console.log(nameShop)
+console.log(date)
+console.log(order)
+console.log(nameUser)
+console.log(barrier)
+console.log(headTable)
+console.log(barrier)
+console.log('1   Cutter        1     169.52  169.52')
+console.log('2   Steel Ruler   2     315.03  630.06')
+console.log('3   Eraser        10    34.00   340.00')
+console.log('4   Pencil Case   1     124.77  124.77')
+console.log(barrier)
+console.log('    Total                     1,264.35')
+console.log(barrier)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- workshop/lesson01/exercise-2/script.js
+++ workshop/lesson01/exercise-2/script.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const country = 'Portugal'
+const continent = 'Europe'
+const population = 40
+const isIsland = false
+let language
+
+console.log(country+'\n'+continent+'\n'+population+'\n'+isIsland);
+
+language = 'portuguese'
+
+console.log(`A half population of ${country} is ${population/2}`)
+console.log(`Population of ${country} is ${population+1}`)
+console.log(`Finland has a population more than ${country} is ${population > 6}`)
+console.log(`${country} have less people than 33 million people that is the average population of a country is ${population < 33}`)
+
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+console.log(description)
+
+if(population > 33){
+    console.log('Portugal is population is above average')
+}
+else {
+    console.log('Portugal is population is 22 million below average')
+
+}
+console.log('9' - '5')
+console.log('19' - '13' + '17')
+console.log('19' - '13' + 17)
+console.log('123' < 57)
+console.log(5 + 6 + '4' + 9 - 4 - 2)
+
+// let numNeighbours = prompt('How many neighbour countries does your country have?')
+// numNeighbours = parseInt(numNeighbours);
+// if(numNeighbours === 1){
+//     console.log('Only 1 border!')
+// }
+// else if(numNeighbours > 1){
+//     console.log( 'More than 1 border')
+// }
+// else {
+//     console.log('No borders')
+// }
+if(population < 50 && !isIsland){
+    console.log('You should live in Portugal :)')
+}
+else{
+    console.log('Portugal does not meet your criteria :(')
+}
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+      console.log('MOST number of native speakers!');
+      break;
+    case 'spanish':
+      console.log('2nd place in number of native speakers');
+      break;
+    case 'english':
+      console.log('3rd place');
+      break;
+    case 'hindi':
+      console.log('Number 4');
+      break;
+    case 'arabic':
+      console.log('5th most spoken language');
+      break;
+    default:
+      console.log('Great language too :D');
+      break;
+}
+
+const ternary  = population > 33 ? `${country}'s population is above average` : `${country}'s population is below average`;
+console.log(ternary)
