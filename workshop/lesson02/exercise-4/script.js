7f74b6e56f7bdcd53e3fa50d3a8bfd4eec3e221e
guymn <gjp2267@gmail.com>
2023-06-21 21:58:44 +0700

    Lesson2 ex.4

--- workshop/lesson02/exercise-4/script.js
+++ workshop/lesson02/exercise-4/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bill = [125, 555, 44]
+const tips = []
+const total = []
+
+for(let i = 0;i < bill.length ;i++){
+    tips[i] = calcTip(bill[i])
+    total[i] = tips[i] + bill[i]
+}
+
+function calcTip(bill){
+    let tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2
+    return tip 
+}
+
+console.log(bill)
+console.log(tips)
+console.log(total)
