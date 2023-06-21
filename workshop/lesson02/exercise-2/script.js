995bede1c614dec4301e9d7ea68c74a65c89e02e
guymn <gjp2267@gmail.com>
2023-06-21 21:37:23 +0700

    Lesson2 ex.2

--- workshop/lesson02/exercise-2/script.js
+++ workshop/lesson02/exercise-2/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const bill = [275, 40, 430]
+for(let i = 0;i < bill.length ;i++){
+    let tip = bill[i] >= 50 && bill[i] <= 300 ? `The bill ias ${bill[i]}, the tip was ${bill[i] * 0.15}, and the total value ${bill[i]+(bill[i]*0.15)}`
+                                              : `The bill ias ${bill[i]}, the tip was ${bill[i] * 0.2}, and the total value ${bill[i]+(bill[i]*0.2)}`
+    console.log(tip)
+}
