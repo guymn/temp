c7078b907fc7f7c12bb561b98698fe97828fdae6
guymn <gjp2267@gmail.com>
2023-06-23 11:18:27 +0700

    index

--- workshop/lesson04/exercise-2/script.js
+++ workshop/lesson04/exercise-2/script.js
@@ -1,1 +1,30 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// Select all three ".show-modal" elements, the result will an array of elements
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+   btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', function(){
+    btnCloseModal.addEventListener('click', closeModal);
+})
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+});
