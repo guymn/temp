4dedd564b96f3f46d4450fdc012a289683fc084e
guymn <gjp2267@gmail.com>
2023-06-28 21:00:44 +0700

    Exercise #4

--- workshop/lesson06/exercise-4/script.js
+++ workshop/lesson06/exercise-4/script.js
@@ -1,18 +1,24 @@
 'use strict';
 
-// ### Hints:
-// - Remember which character defines a new line in the textarea 😉
-// - The solution only needs to work for a variable made out of 2 words, like a_b
-// - Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-// - This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
-
 const textArea = document.createElement('textarea');
 const clickBtn = document.createElement('button');
 
 clickBtn.textContent = 'OK';
 clickBtn.addEventListener('click', function () {
   const value = textArea.value;
-  console.log(value);
+  const lines = value.split('\n');
+
+  for (let line of lines) {
+    const words = line.trim().split('_');
+    let result = words[0];
+
+    for (let i = 1; i < words.length; i++) {
+      const capitalizedWord = words[i][0].toUpperCase() + words[i].slice(1);
+      result += capitalizedWord;
+    }
+
+    console.log(result.padEnd(20));
+  }
 });
 
 document.body.append(textArea);
