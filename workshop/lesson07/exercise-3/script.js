b60545a2120095d50e3a21019c8d5653e7af40a9
guymn <gjp2267@gmail.com>
2023-06-29 11:36:53 +0700

    3

--- workshop/lesson07/exercise-3/script.js
+++ workshop/lesson07/exercise-3/script.js
@@ -11,3 +11,30 @@
 // Test data:
 // - Data car 1: 'BMW' going at 120 km/h
 // - Data car 2: 'Mercedes' going at 95 km/h
+
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    accelerate() {
+        this.speed += 10;
+        console.log(this.make + ' accelerated to ' + this.speed + ' km/h');
+    }
+    brake() {
+        this.speed -= 5;
+        console.log(this.make + ' slowed down to ' + this.speed + ' km/h');
+    }
+}
+
+
+var car1 = new Car('BMW', 120);
+var car2 = new Car('Mercedes', 95);
+
+car1.accelerate(); 
+car1.accelerate(); 
+car1.brake(); 
+
+car2.accelerate();
+car2.brake(); 
+car2.brake(); 
