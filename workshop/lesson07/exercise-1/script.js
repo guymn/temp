2e8da054377228e19c7879f7546adf5c8f06723f
guymn <gjp2267@gmail.com>
2023-06-29 11:19:58 +0700

    1

--- workshop/lesson07/exercise-1/script.js
+++ workshop/lesson07/exercise-1/script.js
@@ -12,7 +12,6 @@
 //      (Write option number)
 //      ```
 //    - Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
-
 // 2. Call this method whenever the user clicks the "Answer poll" button.
 
 // 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
@@ -21,11 +20,40 @@
 
 // 5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
 
+const pollBtn = document.querySelector('.poll');
+
 const poll = {
   question: 'What is your favourite programming language',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    let prom = 'What is your favourite programming language?\n'
+      poll.options.forEach((value) => {
+        prom += value + '\n'
+      }); 
+    const input = prompt(prom)
+    let select = Number(input)
+    if(select >= 0 && select <= 3){
+      poll.answers[select]++
+      this.displayResults(input);
+    }
+    else{
+      this.registerNewAnswer();
+    }
+  },
+  displayResults(type) {
+    if (typeof(type).Array) {
+      console.log(this.answers);
+
+    } else {
+      const resultsString = `Poll results are ${this.answers.join(', ')}`;
+      console.log(resultsString);
+    }
+  }
 };
 
-poll.registerNewAnswer();
+
+pollBtn.addEventListener('click', function(){
+  poll.registerNewAnswer();
+});
