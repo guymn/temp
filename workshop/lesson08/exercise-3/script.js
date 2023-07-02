83c83f887243ef390c1f6f25d7f34536987d425b
guymn <gjp2267@gmail.com>
2023-07-02 14:32:51 +0700

    f

--- workshop/lesson08/exercise-3/script.js
+++ workshop/lesson08/exercise-3/script.js
@@ -9,3 +9,44 @@
 // ### Test data:
 
 // - Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(newSpeed) {
+    this.speed = newSpeed * 1.6;
+  }
+  accelerate() {
+    this.speed += 10;
+    return this;
+  }
+  brake() {
+    this.speed -= 5;
+    return this;
+  }
+}
+class EVCl extends CarCl {
+    #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`
+    );
+    return this;
+  }
+}
+
+const car1 = new EVCl('Rivian', 120, 23);
+car1.accelerate().brake().chargeBattery(90);
