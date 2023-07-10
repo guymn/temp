5fda1fc09a703f373738333bfd1cca01dcccb0b1
guy <gjp2267@gmail.com>
2023-07-10 14:48:03 +0700

    add attri

--- workshop/lesson12/exercise-2/models/tourModel.js
+++ workshop/lesson12/exercise-2/models/tourModel.js
@@ -3,13 +3,24 @@
 const mongoose = require('mongoose');
 
 const toursSchema = mongoose.Schema({
+  id: { type: Number, default: 0 },
   name: {
     type: String,
     required: [true, 'A tour must name a name'],
     unique: true,
   },
+  duration: { type: Number, default: 0 },
+  maxGroupSize: { type: Number, default: 0 },
+  difficulty: { type: String, default: "--" },
+  ratingsAverage: { type: Number, default: 0.0 },
+  ratingsQuantity: { type: Number, default: 0 },
   rating: { type: Number, default: 4.5 },
   price: { type: Number, required: [true, 'A tour must price'] },
+  summary: { type: String, default: "--" },
+  description: { type: String, default: "--" },
+  imageCover: { type: String, default: './img/img.png' },
+  images: { type: Array, default: ['./img/img2.png', './img/img3.png'] },
+  startDates: { type: Array, default: ["0000-00-00, 00:00"] },
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
