bdd042623dcb9007d3ade147033f17749bf64e16
guy <gjp2267@gmail.com>
2023-07-23 22:48:46 +0700

    mearge app tours and users

--- workshop/lesson12/exercise-2/app.js
+++ workshop/lesson12/exercise-2/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const tourRoute = require('./tour/tourRoutes');
+const userRoute = require('./user/userRoutes');
 const { escape } = require('querystring');
 
 const app = express();
 app.use(express.json());
 app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/users', userRoute);
 app.use(express.static(`${__dirname}/public`));
 
 module.exports = app;
